696128c3cdb59d7ea3184f407701b3d8193a5857
noname2103 <35282818+noname2103@users.noreply.github.com>
2019-03-24 14:10:38 +0700

    giao diện
    giao diện

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+  // Hien trang chu
+  public function trangchu()
+  {
+    return view('user_interface.index');
+  }
+
+  // Hien giao dien trang dang ky
+  public function getdangky()
+  {
+    return view('user_interface.register');
+  }
+
+  // Hien giao dien trang dang nhap
+  public function getdangnhap()
+  {
+    return view('user_interface.login');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('user_interface.index');
 });
+
+// Hien trang dang ky nguoi dung
+Route::get('dang-ky.html','UserController@getdangky')->name('getdangky');
+// Xu ly thong tin dang ky thanh vien
+Route::post('dang-ky.html','UserController@postdangky')->name('postdangky');
+// Hien trang dang nhap nguoi dung
+Route::get('dang-nhap.html','UserController@getdangnhap')->name('getdangnhap');
+// Xu ly thong tin dang nhap thanh vien
+Route::post('dang-nhap.html','UserController@postdangnhap')->name('postdangnhap');
+// Hien trang gio hang
+Route::get('gio-hang.html','UserController@giohang')->name('giohang');
+// Hien trang xem chi tiet san pham
+Route::get('{slug}&p{mabv}.html','UserController@xemsanpham')->name('xemsanpham');
